d065d9bd5caef1e3ca9983a8caf29afe0bc30f30
Nhattran <nhatkophanua@gmail.com>
2025-03-15 10:08:45 +0700

    Delete logout.php

--- logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-session_destroy();
-setcookie("username", "", time() - 3600, "/");
-setcookie("email", "", time() - 3600, "/");
-setcookie("password", "", time() - 3600, "/");
-header("Location: ./login.php");
-exit();
-?>
